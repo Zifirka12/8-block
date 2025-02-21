9ef2d0eedc0deb65274674e9ac131941ecedcc77
Student <Ilaafanasef21@gmail.com>
2025-02-21 16:17:15 +0300

    30/1

---
+++ zifirkaguide/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from zifirkaguide.views import CourseViewSet, LessonCreateApiView, LessonUpdateApiView, LessonRetrieveApiView, \
+    LessonDestroyApiView, LessonListApiView
+from zifirkaguide.apps import ZifirkaguideConfig
+
+app_name = ZifirkaguideConfig.name
+
+router = SimpleRouter()
+router.register("", CourseViewSet)
+
+urlpatterns = [
+    path("lessons/", LessonListApiView.as_view(), name="lessons_list"),
+    path("lessons/<int:pk>/", LessonRetrieveApiView.as_view(), name="lessons_retrieve"),
+    path("lessons/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lessons_update"),
+    path("lessons/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="lessons_delete"),
+    path("lessons/create/", LessonCreateApiView.as_view(), name="lessons_create"),
+]
+
+urlpatterns += router.urls
